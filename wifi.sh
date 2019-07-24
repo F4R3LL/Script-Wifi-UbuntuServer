36dd8b8a73b90b8744fd1a80e947646c3fa33b59
Fernandes <val92.fernandes@gmail.com>
2019-07-24 11:36:27 +0200

    v1.0 Wifi Script

--- wifi.sh
+++ wifi.sh
@@ -2,17 +2,17 @@
 
 # Scan Wifi / Display only Essid
 iwlist scan | grep -i essid | sort -u
-echo "\n\t\t ------ Configuration Wifi ------\n"
+echo "\n\t\t ------ Configuration Wifi --- SAVE A BACKUP BEFORE ANY CHANGE ---\n"
 read -p "[Wifi] Identifiant (Essid) : " var1
 read -p "[Wifi] Mot de passe : " var2
 
 # Remove essid & password
-# /!\ (Nothing is written after essid/pwd for me so be carefull if it's not your case you may lose your data) /!\
-# Replace 13 and 14 here to match with your file /etc/network/interfaces 
-sed -i '13,14d' /etc/network/interfaces
+# 
+# Line 5 = essid / Line 6 = password
+sed -i '5,6d' /etc/network/interfaces
 
 # Refresh /etc/network/interface
-# Generate ID and Crypted Password and format the output  for etc/network/interfaces
-# Run "service networking reset" or reboot your system after this
-wpa_passphrase $var1 $var2 | grep -vE "{|#|}" | tr '=' ' ' | tr -d '\t' >> /etc/network/interfaces && sed -i '13s/ssid/wpa_ssid/' /etc/network/interfaces && sed -i '14s/psk/wpa_psk/' /etc/network/interfaces
+# Generate an ID and a Crypted Password to etc/network/interfaces
+# Run "service networking reload" or reboot your system after this
+wpa_passphrase $var1 $var2 | grep -vE "{|#|}" | tr '=' ' ' | tr -d '\t' >> /etc/network/interfaces && sed -i '5s/ssid/wpa_ssid/' /etc/network/interfaces && sed -i '6s/psk/wpa_psk/' /etc/network/interfaces
 echo "\n/etc/network/interfaces : Updated!"
